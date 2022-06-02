9b3dce8ea7131b68a877d10b0fc29594993c07ce
hporrnhubtu <106743394+hporrnhubtu@users.noreply.github.com>
2022-06-02 19:50:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bae à!',
+    introDesc: `Tính toai ló vốn lạ thường
+    Má sao bạn lại dỗi toai nhể
+    Tooiim ,bạn im,thế là im 
+    Đằng nào cx dỗi thì thôi,chả thôi `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Phải chăng bạn đã dỗi tôi :(((',
+    desc: 'Phải chăng bạn đã hết muốn chơi dồi ',
+    btnYes: 'hết dồi :(((',
+    btnNo: 'Không, bạn à  :3',
+    question:'Tôi biết chả ai chịu đc tính chó đẻ của tôi đâu :((',
+    btnReply: 'Hình như bạn ốm uống thuốc đi nhá <3',
+    reply: ' chơi thì chơi ko chơi thì... ',
+    mess: 'uống thuốc đi 🥰. Yêu rất nhiều em  😘😘',
+    messDesc: 'Sáng mai 7h Ngọ  qua đón nhé A giang .',
+    btnAccept: 'ò ò ò ò ò ',
+    messLink: ' https://www.facebook.com/profile.php?id=100034854194827.com'
 }
